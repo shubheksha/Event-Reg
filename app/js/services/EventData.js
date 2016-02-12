31fb9c0de20d779d206f0dd84ae0e9d8b7264ad2
Shubheksha Jalan <shubheksha@outlook.com>
2016-02-12 13:26:32 +0530

    Added Edit Profile page & Event Data service

---
+++ app/js/services/EventData.js
@@ -0,0 +1,40 @@
+eventsApp.factory('eventData', function(){
+  return{
+    event: {
+      name: 'AngularJS Fundamentals',
+      date: '2/10/2016',
+      time: '6:00 PM',
+      location: {
+        address: 'Google Headquaters',
+        city: 'Mountain View',
+        province: 'CA'
+      },
+      imgUrl: 'img/angularjs-logo.png',
+      sessions: [
+        {
+          name: 'Directives Masterclass',
+          creatorName: 'Bob Smith',
+          duration: 1,
+          level: 'Advanced',
+          upvoteCount: 0
+        },
+        {
+          name: 'Scope for Fun and Profit',
+          creatorName: 'John Doe',
+          duration: 2,
+          level: 'Introductory',
+          upvoteCount: 0
+        },
+        {
+          name: 'Well Behaved Controllers',
+          creatorName: 'Jane Doe',
+          duration: 4,
+          level: 'Intermediate',
+          upvoteCount: 0
+
+        }
+      ]
+  }
+
+  }
+});
